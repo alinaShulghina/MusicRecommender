0c3caf8a45b45218b6ca912628a060fe536a1521
Alina Shulhina <ashulhina@atlassian.com>
2018-05-02 18:44:21 +0300

    Initialization commit

---
+++ src/main/java/com/diit/music/reccomend/ReccomendApplication.java
@@ -0,0 +1,12 @@
+package com.diit.music.reccomend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReccomendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReccomendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/diit/music/reccomend/ReccomendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.diit.music.reccomend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReccomendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
